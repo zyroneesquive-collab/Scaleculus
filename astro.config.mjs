52029dcd1f8ae134d0386d61ddbae0da9c9a4ab9
n0cturneler <shizucl0ak@gmail.com>
2026-03-05 15:29:54 +0800

    config edit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://n0cturneler.github.io',
-  base: '/Scaleculus',
+  base: '/Scaleculus/',
 
   vite: {
     plugins: [tailwindcss()]
