7750aff6e24d100233b3954c30dcf782cd03e4c1
n0cturneler <shizucl0ak@gmail.com>
2026-03-15 12:01:09 +0800

    change2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import starlight from '@astrojs/starlight';
 import tailwindcss from '@tailwindcss/vite';
+
+import markdoc from '@astrojs/markdoc';
 
 // https://astro.build/config
 export default defineConfig({
   base: '/Scaleculus',
   site: 'https://n0cturneler.github.io/Scaleculus/',
   output: 'static',
+    integrations: [starlight({
+      title: 'Scaleculus',
+            customCss: [
+        // Relative path to your custom CSS file
+        './src/styles/global.css',
+      ],
+    })],
 });
---
+++ src/content/lesson_text/limits.mdx
@@ -0,0 +1,13 @@
+import { Steps } from '@astrojs/starlight/components';
+
+<Steps>
+
+1. Import the component into your MDX file:
+
+   ```js
+   import { Steps } from '@astrojs/starlight/components';
+   ```
+
+2. Wrap `<Steps>` around your ordered list items.
+
+</Steps>
